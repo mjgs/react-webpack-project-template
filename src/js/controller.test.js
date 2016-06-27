b11484da15d56e6007572493b445c75afafd6453
Mark Smith <markjgsmith@gmail.com>
2016-06-27 18:16:05 +0700

    Update tests to check that controller exists

--- src/js/controller.test.js
+++ src/js/controller.test.js
@@ -1,10 +1,7 @@
 import Controller from './controller.js'
 
 describe('test', () => {
-  it('works', () => {
-    expect(true).to.be.true;
-  });
-  it('works', () => {
-    expect('hi').to.equal('hi');
+  it('exists', () => {
+    expect(Controller).to.exist;
   });
 });
